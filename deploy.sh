2b1b59367e300a0a6022dfb337ce7bb8012886fa
orangemug <info@orangemug.co.uk>
2016-10-01 21:32:18 +0100

    More robust deploy steps

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if [[ -n $(git status -s) ]]; then
+  >&2 echo "Error: Working directory not clean"
+  exit 1
+fi
+
+>&2 echo "Generating glyphs"
 ./generate.sh
 
 SHA=`git rev-parse --verify HEAD`
@@ -9,7 +15,12 @@ git checkout gh-pages || git checkout --orphan gh-pages
 rm .git/index
 
 git add glyphs
-git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-git push origin gh-pages
+if [ -n "`git diff --staged`" ]; then
+  git commit -m "Deploy to GitHub Pages: ${SHA}"
+  git push origin gh-pages
+else
+  >&2 echo "Nothing to deploy"
+fi
+
 git checkout master -f
