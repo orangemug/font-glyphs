cdd5e9c2fe7b61b4e38ba2dfbd63b79746adc04e
orangemug <info@orangemug.co.uk>
2016-10-05 10:49:21 +0100

    Added initial example

---
+++ example/app.js
@@ -0,0 +1,58 @@
+var map = new mapboxgl.Map({
+  container: 'map',
+  center: [8.538961,47.372476],
+  zoom: 5,
+  hash: true
+});
+
+var style = fetch("osm-liberty.json")
+  .then(function(resp) {
+    return resp.json()
+  })
+
+style.then(function(data) {
+  map.setStyle(data);
+});
+
+var glyphs = fetch("/glyphs/index.json")
+  .then(function(resp) {
+    return resp.json()
+  })
+  .then(function(data) {
+    var styles = [];
+    data.fonts.forEach(function(_font) {
+      _font.styles.forEach(function(_style) {
+        styles.push(_style);
+      })
+    })
+
+    var selectEl = document.querySelector("select");
+    styles.forEach(function(_style) {
+      var optEl = document.createElement("option");
+      optEl.innerText = _style.name;
+      optEl.setAttribute("value", _style.name);
+      selectEl.appendChild(optEl);
+    })
+
+    selectEl.addEventListener("change", function() {
+      if(selectEl.value) {
+        changeStyle(selectEl.value);
+      }
+    })
+
+  })
+
+function changeStyle(name) {
+  style
+    .then(function(_style) {
+      _style.layers.forEach(function(_layer) {
+        if(_layer.layout && _layer.layout.hasOwnProperty("text-font")) {
+          map.setLayoutProperty(_layer.id, "text-font", [
+            name
+          ]);
+        }
+      })
+    })
+}
+
+map.addControl(new mapboxgl.Navigation());
